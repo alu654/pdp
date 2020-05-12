4b1a28307b69c05fb553af6140f096811073a085
alu654 <alanffederico@hotmail.com>
2020-05-12 16:01:20 -0300

    Correccion 1

--- correcciontp3.hs
+++ correcciontp3.hs
@@ -20,20 +20,23 @@ manuel = UnParticipante "Manuel" 500 "Oferente singular" [] [pasarPorElBanco, en
 -- pasarPorElBanco: aumenta el dinero del jugador en $40 y cambia su táctica a “Comprador compulsivo”.
 
 pasarPorElBanco :: Accion
-pasarPorElBanco unParticipante = unParticipante {cantidadDeDinero  =  cantidadDeDinero  unParticipante + 40 , tacticaDeJuego = "Comprador compulsivo" }
+pasarPorElBanco unParticipante  = unParticipante {cantidadDeDinero  =  cambiarDinero unParticipante 40 , tacticaDeJuego = "Comprador compulsivo" }
 
 -- enojarse: suma $50 y agrega gritar a sus acciones.
 
 enojarse :: Accion
-enojarse unParticipante =  unParticipante {cantidadDeDinero  =  cantidadDeDinero  unParticipante + 50 , accionesDelJuego = accionesDelJuego unParticipante ++ [gritar]} 
+enojarse unParticipante =  unParticipante {cantidadDeDinero  =  cambiarDinero unParticipante 50 , accionesDelJuego = accionesDelJuego unParticipante ++ [gritar]} 
 
+
+cambiarDinero :: Participante -> Int->  Int --500 40 = 540 
+cambiarDinero unParticipante dinero = (cantidadDeDinero unParticipante + dinero ) 
 -- gritar: agrega “AHHHH” al principio de su nombre.
 
 gritar :: Accion 
 gritar unParticipante = unParticipante { nombre  = "AHHHH" ++ nombre unParticipante }
 
 
--- subastar: al momento de una subasta solo quienes tengan como tácticas “Oferente singular” o “Accionista” podrán ganar la propiedad. Ganar implica restar el precio de la propiedad de su dinero y sumar la nueva adquisición a sus propiedades. 
+-- subastar: al momento de una subasta solo quienes tengan como tácticas “Oferente singular” o “Accionista” podrán ganar la propiedad. Ganar implica precio el precio de la propiedad de su dinero y sumar la nueva adquisición a sus propiedades. 
 
 subastar :: Propiedad -> Accion
 subastar adquirida unParticipante 
@@ -41,14 +44,13 @@ subastar adquirida unParticipante
     | otherwise = unParticipante
 
 agregarPropiedad :: Propiedad -> Accion
-agregarPropiedad ganada unParticipante = unParticipante { cantidadDeDinero = cantidadDeDinero unParticipante - restar ganada, propiedadesCompradas = propiedadesCompradas unParticipante ++ [ganada] }
+agregarPropiedad esAccionistaganada unParticipante = unParticipante { cantidadDeDinero = cantidadDeDinero unParticipante - precio esAccionistaganada, propiedadesCompradas = propiedadesCompradas unParticipante ++ [esAccionistaganada] }
 
-restar:: Propiedad-> Int
-restar (_, plata)= plata 
+precio:: Propiedad-> Int
+precio (_, plata)= plata 
 
 esAccuOferente :: Participante -> Bool
-esAccuOferente  unParticipante = tacticaDeJuego unParticipante == "Oferente singular" || tacticaDeJuego unParticipante == "Accionista"
-
+esAccuOferente  unParticipante = esDe "Oferente singular" || esDe "Accionista"
 -- cobrarAlquileres: suma $10 por cada propiedad barata y $20 por cada propiedad cara obtenida. Las propiedades baratas son aquellas cuyo precio es menor a $150.
 
 cobrarAlquileres :: Accion
@@ -63,8 +65,12 @@ unaPropiedadBarata unaPropiedad
 
 pagarAAccionistas :: Accion
 pagarAAccionistas unParticipante 
-    |restaAccionistas unParticipante = unParticipante {cantidadDeDinero = cantidadDeDinero unParticipante + 200}
+    |esAccionista unParticipante = unParticipante {cantidadDeDinero = cambiarDinero unParticipante (200)}
     |otherwise = unParticipante {cantidadDeDinero = cantidadDeDinero unParticipante - 100}
 
-restaAccionistas :: Participante -> Bool
-restaAccionistas  unParticipante = tacticaDeJuego unParticipante == "Accionista" 
+esAccionista :: Participante -> Bool
+esAccionista  tactica = esDe "Accionista"
+
+
+esDe :: String -> Bool
+esDe tactica = tactica == "Oferente singular" || tactica  == "Accionista" 
